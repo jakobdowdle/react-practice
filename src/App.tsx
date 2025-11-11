68363c849ebdc7903aa078677c782e619ea00146
Jakob Dowdle <jakobrdowdle@gmail.com>
2025-11-10 19:59:43 -0700

    making a network request and tracking loading, error, and success using useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<Error | null>(null)
+  const [data, setData] = useState(null)
+
+  const fetchPikachu = async () => {
+    setData(null)
+    setError(null)
+    setLoading(true)
+    try {
+      const request = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+
+      if (!request.ok) {
+        setError(new Error('Failed to fetch data'))
+        return
+      }
+      const data = await request.json()
+      setData(data)
+
+      return data
+    } catch (error) {
+        setError(error instanceof Error ? error : new Error(String(error)))
+    } finally {
+        setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchPikachu().then(data => console.log(data))
+  }, [])
 
   return (
     <>
