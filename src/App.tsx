0a2c1bba10a0ccf5eb4b9491a9518530146687ed
Jakob Dowdle <jakobrdowdle@gmail.com>
2025-11-10 20:23:20 -0700

    created pokemon type and displayed some pokemon information to screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,156 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+type Pokemon = {
+    id: number
+    name: string
+    base_experience: number
+    height: number
+    weight: number
+    is_default: boolean
+    order: number
+    location_area_encounters: string
+    abilities: Array<{
+      ability: {
+        name: string
+        url: string
+      }
+      is_hidden: boolean
+      slot: number
+    }>
+    cries: {
+      latest: string
+      legacy: string
+    }
+    forms: Array<{
+      name: string
+      url: string
+    }>
+    game_indices: Array<{
+      game_index: number
+      version: {
+        name: string
+        url: string
+      }
+    }>
+    held_items: Array<{
+      item: {
+        name: string
+        url: string
+      }
+      version_details: Array<{
+        rarity: number
+        version: {
+          name: string
+          url: string
+        }
+      }>
+    }>
+    moves: Array<{
+      move: {
+        name: string
+        url: string
+      }
+      version_group_details: Array<{
+        level_learned_at: number
+        move_learn_method: {
+          name: string
+          url: string
+        }
+        version_group: {
+          name: string
+          url: string
+        }
+      }>
+    }>
+    past_abilities: Array<{
+      ability: {
+        name: string
+        url: string
+      }
+      slot: number
+    }>
+    past_types: Array<{
+      generation: {
+        name: string
+        url: string
+      }
+      types: Array<{
+        slot: number
+        type: {
+          name: string
+          url: string
+        }
+      }>
+    }>
+    species: {
+      name: string
+      url: string
+    }
+    sprites: {
+      back_default: string | null
+      back_female: string | null
+      back_shiny: string | null
+      back_shiny_female: string | null
+      front_default: string | null
+      front_female: string | null
+      front_shiny: string | null
+      front_shiny_female: string | null
+      other: {
+        dream_world: {
+          front_default: string | null
+          front_female: string | null
+        }
+        home: {
+          front_default: string | null
+          front_female: string | null
+          front_shiny: string | null
+          front_shiny_female: string | null
+        }
+        'official-artwork': {
+          front_default: string | null
+          front_shiny: string | null
+        }
+        showdown: {
+          back_default: string | null
+          back_female: string | null
+          back_shiny: string | null
+          back_shiny_female: string | null
+          front_default: string | null
+          front_female: string | null
+          front_shiny: string | null
+          front_shiny_female: string | null
+        }
+      }
+      versions: Record<string, any>
+    }
+    stats: Array<{
+      base_stat: number
+      effort: number
+      stat: {
+        name: string
+        url: string
+      }
+    }>
+    types: Array<{
+      slot: number
+      type: {
+        name: string
+        url: string
+      }
+    }>
+}
+
+function toCapitalized(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState<Error | null>(null)
-  const [data, setData] = useState(null)
+  const [data, setData] = useState<Pokemon | null>(null)
   const [success, setSuccess] = useState(false)
 
   const fetchPikachu = async () => {
@@ -23,7 +167,7 @@ function App() {
         setError(new Error('Failed to fetch data'))
         return
       }
-      const data = await request.json()
+      const data: Pokemon = await request.json()
       setSuccess(true)
       setData(data)
 
@@ -42,25 +186,20 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {loading && <p>loading...</p>}
+        {error && <p>error: {error.message}</p>}
+        {data && (
+          <div>
+            {data.sprites.front_default && (
+              <img src={data.sprites.front_default} alt={data.name} />
+            )}
+            <h1>{toCapitalized(data.name)}</h1>
+            <h3>id: {data.id}</h3>
+            <h2>Height: {data.height}</h2>
+            <h2>Weight: {data.weight}</h2>
+          </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
